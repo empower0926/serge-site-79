114e19eb5072512802eff6025aa30415b04bab24
haaamza <hamzagee@outlook.com>
2022-11-12 08:07:00 +0000

    Build: (4efbd87) Update publish.yml

---
+++ precache-manifest.3049d418a846d5562c52b8f7e8ed7a91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72eaa769f64da1a58b1d3b4e3bc550df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb75d8751c29e09c6a4f",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "acab24e8b226362d5790",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb75d8751c29e09c6a4f",
+    "url": "/static/js/2.a2a811e8.chunk.js"
+  },
+  {
+    "revision": "73540b2ebd718bf52f14fb64ec5fae9b",
+    "url": "/static/js/2.a2a811e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acab24e8b226362d5790",
+    "url": "/static/js/main.9cc8ef3a.chunk.js"
+  },
+  {
+    "revision": "180f3bba962e8ea7d4a8",
+    "url": "/static/js/runtime-main.7347e521.js"
+  }
+]);
--- src/models/transaction/transaction_completebtn.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export const CompleteBtn = (props) =>{
-    return(
-        <span className="my-auto ml-auto span-completed">Completed <img className="ml-3"
-                            src="../assets/images/exported/checked.svg" alt=""/></span>
-    );
-}
